d6aca2ac6971677cb099efab1c854c502fec4289
samantha <samantha.pangracio@escola.pr.gov.br>
2025-07-30 08:02:57 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Ginástica Ritmica',
+    'Quantos aparelhos existem na ginastica?',
+    '5 aparelhos'
+)
+
+criaCartao (
+    'Ginástica Ritmica',
+    'Quanto tempo tem uma serie de individual?',
+    '1 minuto e 30 segundos'
+)
+
+criaCartao (
+    'Ginástica Rítmica',
+    'Quanto tempo tem uma serie de conjunto?',
+    '2 minutos e meio'
+)
