18a9f2d7c039899e237fdfbbffb558c20d7bbea9
samantha <samantha.pangracio@escola.pr.gov.br>
2025-08-13 07:37:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ function criaCartao(categoria, pergunta, resposta) {
         let cartao = document.createElement('article')
         cartao.className = 'cartao'
 
-   cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-        <p>${pergunta}</p>
+   cartao.innerHTML = ` <div class="cartao__conteudo">
+    <h3>Ginástica Rítmica</h3>
+    <div class="cartao__conteudo__pergunta"> 
+    <p>Quantos aparelhos existem na ginastica?</p>
     </div>
     <div class="cartao__conteudo__resposta">
-        <p>${resposta}</p>
+    <p>5 aparelhos.</p>
     </div>
     </div>
     `
