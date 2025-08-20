708d46c223e15ddbe83e80f491797f66106269b9
samantha <samantha.pangracio@escola.pr.gov.br>
2025-08-20 07:52:53 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,35 @@
 criaCartao (
     'Ginástica Ritmica',
     'Quantos aparelhos existem na ginastica?',
-    '5 aparelhos'
+    '5 aparelhos.'
 )
 
 criaCartao (
     'Ginástica Ritmica',
     'Quanto tempo tem uma serie de individual?',
-    '1 minuto e 30 segundos'
+    '1 minuto e 30 segundos.'
 )
 
 criaCartao (
     'Ginástica Rítmica',
     'Quanto tempo tem uma serie de conjunto?',
-    '2 minutos e meio'
+    '2 minutos e meio.'
+)
+
+criaCartao (
+    'Conhecimentos gerais',
+    'Que país tem o formato de uma bota?',
+    'Itália.'
+)
+
+criaCartao (
+    'Conhecimentos gerais',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison.'
+)
+
+criaCartao (
+    'Conhecimentos gerais',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter.'
 )
